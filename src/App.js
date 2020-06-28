561ab9fd8f4856962a200f174d425cc082088cae
孙菁 <sunjing@sunjingdeMacBook-Pro.local>
2020-06-28 22:32:41 +0800

    react expression

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+let jsx = <p>hello,react</p>
+ 
+function formatName (user) {
+  return `${user.firstName},${user.lastName}`
+}
 
 function App() {
+  const NAME = 'jerry'
+  const USER = {firstName: 'tom', lastName: 'jerry'}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 表达式 */}
+      <h1>{NAME}</h1>
+      <h2>{formatName(USER)}</h2>
+
+      {/* 属性 */}
+      <img src={logo} style={{width: '200px'}} alt="" />
+
+      {/* jsx也是表达式 */}
+      {jsx}
     </div>
   );
 }
