0d3049b47f17302026d89c83b212c9c8ab61d2d0
孙菁 <sunjing@hoge.cn>
2020-07-08 14:43:33 +0800

    shouldComponentUpdate PureComponent memo

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 export class CommentList extends React.Component {
   constructor(props) {
-    console.log('CommentList constructor')
+    // console.log('CommentList constructor')
     super(props)
     this.state = {
-      comments: []
+      comments: [],
+      tempData: '12345'
     }
   }
 
   componentDidMount () {
-    setTimeout(() => {
+    setInterval(() => {
       this.setState({
         comments: [
           {body: 'react is very good', author: 'facebook'},
@@ -27,36 +28,66 @@ export class CommentList extends React.Component {
   }
 
   render () {
-    console.log('CommentList render')
+    // console.log('CommentList render')
     return (
       <div>
         {this.state.comments.map((c, i) => (
-          <Comment key={i} data={c}></Comment>
+          // 这里扩展相当于： <Comment key={i} body={c.body} author={c.author}></Comment>
+          <Comment key={i} {...c}></Comment>
         ))}
       </div>
     )
   }
 }
 
+// 避免组件频繁更新1 - shouldComponentUpdate
 class Comment extends React.Component {
 
   shouldComponentUpdate (nextProps) {
-    if(nextProps.data.body === this.props.data.body &&
-      nextProps.data.author === this.props.data.author) {
+    if(nextProps.body === this.props.body &&
+      nextProps.author === this.props.author) {
         return false;
     }
     return true;
   }
 
   render () {
-    // 打印2次render
-    // 首次加载组件执行一次，当父组件componentDidMount中修改comments，父组件state发生变化又执行一次
-    // console.log('Comment render')
+    // 打印4次
+    console.log('Comment render')
     return (
       <div>
-        <p>{this.props.data.body}</p>
-        <p>-- {this.props.data.author}</p>
+        <p>{this.props.body}</p>
+        <p>-- {this.props.author}</p>
       </div>
     )
   }
 }
+
+// 避免组件频繁更新2 - PureComponent 
+// PureComponent内置shouldComponentUpdate只做浅比较
+// 避免对象传值，对象传值yeoman特别留心引用的问题，尽量传数值类型的数据
+// class Comment extends React.PureComponent {
+//   render () {
+//     // 打印4次
+//     console.log('Comment render')
+//     return (
+//       <div>
+//         <p>{this.props.body}</p>
+//         <p>-- {this.props.author}</p>
+//       </div>
+//     )
+//   }
+// }
+
+// 避免组件频繁更新3 - React.memo 
+// 类似PureComponent只做浅比较
+// const Comment = React.memo(function (props) {
+//   // 打印2次
+//   console.log('Comment render')
+//   return (
+//     <div>
+//       <p>{props.body}</p>
+//       <p>-- {props.author}</p>
+//     </div>
+//   )
+// })
