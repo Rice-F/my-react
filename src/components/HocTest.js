da40a4aba019fec37590c355e7affdfc53958bce
孙菁 <sunjing@sunjingdeMacBook-Pro.local>
2020-07-17 14:56:09 +0800

    hook

--- src/components/HocTest.js
+++ src/components/HocTest.js
@@ -1,4 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+
+// 自定义hook是个函数，名称用use开头即可，函数内部可以调用其他hook
+function useAge () {
+  const [age, setAge] = useState(0)
+  useEffect(() => {
+    setTimeout(() => {
+      setAge(20)
+    }, 2000)
+  })
+  return age;
+}
 
 export default function HocTest () {
 
@@ -6,7 +17,27 @@ export default function HocTest () {
   // useState(initialState)
   const [count, setCount] = useState(0)
 
-  const [age] = useState(20)
+  // 每次渲染时都执行
+  // 可以理解为class类组件中的componentDidMount、componentDidUpdate、componentwillUnmount的合并api
+  useEffect (() => {
+    document.title =  `你点击了${count}次`
+  })
+
+  // useEffect会在每次组件都渲染时调用，那么如果需要在此方法内部调用api时，频繁调用api就不合理了
+  // 这时可以给useEffect方法传递第二个参数，为[]
+  useEffect (() => {
+    // ...api调用
+    console.log('api调用')
+  }, [])
+
+  // useEffect还可以根据依赖决定自身是否每次渲染都调用，如果依赖没有变，那渲染也是没有意义的
+  // 这时给useEffect方法传递第二个参数，传依赖
+  useEffect (() => {
+    document.title =  `你点击了${count}次`
+  }, [count])
+
+  // const [age] = useState(20)
+  const age = useAge()
   const [fruit, setFruit] = useState('Lemon')
   const [input, setInput] = useState('')
   const [fruits, setFruits] = useState(['apple', 'pear'])
@@ -16,7 +47,7 @@ export default function HocTest () {
         <p>点击了{count}</p>
         <button onClick={() => setCount(count + 1)}>点击</button>
 
-        <p>年龄：{age}</p>
+        <p>年龄：{age ? age : 'loading...'}</p>
         <p>选择的水果：{fruit}</p>
 
         <p>
